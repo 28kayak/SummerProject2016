2f5b59e1007d32ab9f5e8ce261aaa375f8eec0ad
28kayak <kaya.ota@sjsu.edu>
2016-06-03 14:41:08 -0700

    init

---
+++ apiai/samples/main.js
@@ -0,0 +1,15 @@
+var apiai = require('apiai');
+ 
+var app = apiai("462e5c29c3a34ce8afb5e369d6a17c1d");
+ 
+var request = app.textRequest('turn off the light');
+ 
+request.on('response', function(response) {
+    console.log(response);
+});
+ 
+request.on('error', function(error) {
+    console.log(error);
+});
+ 
+request.end()
